a09da3821da7f2d03adce75e2da73194784b7411
Ryan Dale <rd01748@georgiasouthern.edu>
2019-04-25 18:30:44 -0400

    Connect to MongoDB. Listen for app on env filed defined port.

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 
-console.log(process.env.PORT);
+const app = express();
+const port = process.env.PORT;
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true
+}).catch(err => {
+    console.log('Error connecting to MongoDB', err);
+});
+
+app.listen(port, () => {
+    console.log(`Server started on: ${port}`);
+});
