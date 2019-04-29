68b1c77bbe08681f6c2fc9500f8dfb1b322f8332
Ryan Dale <rd01748@georgiasouthern.edu>
2019-04-28 23:24:46 -0400

    Prepare to deploy UI to Heroku.

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, {
 app.use('/api/accounts', require('./routes/accounts'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (request, response) => {
+        response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.listen(port, () => {
     console.log(`Server started on: ${port}`);
 });
